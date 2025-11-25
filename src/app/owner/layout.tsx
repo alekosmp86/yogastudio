3679e1fd2e5d95c698d68a149a3cbb725a765c93
Alejandro Matos <alejandro.matos@doceleguas.com>
2025-11-24 21:00:48 -0300

    Added breadcrumbs component and fixed owner layout

--- src/app/owner/layout.tsx
+++ src/app/owner/layout.tsx
@@ -2,13 +2,15 @@
 
 import { Roles } from "@/enums/Roles";
 import { withRole } from "@/lib/withRole";
+import Breadcrumbs from "@/components/shared/Breadcrumbs";
 
 function OwnerLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen px-6 py-10 bg-brand-800">
-      <h1 className="text-3xl font-bold text-brand-500 mb-6">
+    <div className='min-h-screen px-6 py-10 bg-brand-800'>
+      <h1 className='text-3xl font-bold text-brand-500 mb-6'>
         Owner Dashboard
       </h1>
+      <Breadcrumbs />
       {children}
     </div>
   );
