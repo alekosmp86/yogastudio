e58adab6315df0b64a521564a047abac34fb7201
Alejandro Matos <alejandro.matos@doceleguas.com>
2025-11-28 02:04:40 -0300

    Improved login page

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Input from "@/components/shared/Input";
 import Button from "@/components/shared/Button";
 import { http } from "@/lib/http";
@@ -15,40 +15,43 @@ enum Status {
   ERROR = "error",
 }
 
+const messages: Record<Status, string> = {
+  [Status.ERROR]: "Something went wrong. Try again later.",
+  [Status.SENT]: "A login link has been sent to your email.",
+  [Status.IDLE]: "",
+  [Status.LOADING]: "Requesting link...",
+};
+
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
+  const emailInput = useRef<HTMLInputElement>(null);
   const [status, setStatus] = useState<Status>(Status.IDLE);
-  const [message, setMessage] = useState("");
+  const messageUI = messages[status];
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     setStatus(Status.LOADING);
-    setMessage("");
 
     try {
-      const {message} = await http.get<ApiResponse<string>>(`/auth/request-link?email=${email}`, ApiType.FRONTEND);
+      const {message} = await http.get<ApiResponse<string>>(`/auth/request-link?email=${emailInput.current?.value}`, ApiType.FRONTEND);
 
       if (message !== RequestStatus.REQUEST_LINK_SENT) {
         setStatus(Status.ERROR);
-        setMessage("Unable to process request");
         return;
       }
 
       setStatus(Status.SENT);
-      setMessage("A login link has been sent to your email.");
     } catch (err) {
       setStatus(Status.ERROR);
-      setMessage("Something went wrong. Try again later.");
     }
   }
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen p-6 bg-[#0f151b] text-[#d6d6da]'>
+    <div className='flex flex-col items-center justify-center min-h-screen p-6 bg-surface-app text-brand-200'>
       <form
         onSubmit={handleSubmit}
-        className='flex flex-col gap-4 w-full max-w-xs bg-[#23283f] p-6 rounded-lg shadow-xl border border-[#434447]'
+        className='flex flex-col gap-4 w-full max-w-xs bg-surface-section p-6 rounded-lg shadow-xl border border-brand-600'
       >
-        <h1 className='text-xl font-semibold text-center text-[#d6d6da]'>
+        <h1 className='text-xl font-semibold text-center text-brand-200'>
           Log in
         </h1>
 
@@ -57,26 +60,25 @@ export default function LoginPage() {
           name='email'
           type='email'
           placeholder='you@example.com'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          ref={emailInput}
           required
         />
 
         <Button
           type='submit'
           disabled={status === Status.LOADING}
-          className='p-2 rounded-md bg-[#6d6766] text-[#d6d6da] hover:bg-[#9e9a9f] disabled:opacity-50 transition'
+          variant="primary"
         >
           {status === Status.LOADING ? "Sending..." : "Get link"}
         </Button>
 
-        {message && (
+        {messageUI && (
           <p
             className={`text-sm text-center ${
               status === Status.ERROR ? "text-red-400" : "text-green-400"
             }`}
           >
-            {message}
+            {messageUI}
           </p>
         )}
       </form>
