d31d1e9c7668b0edb5eda8973c6594a0f0d666e0
Alejandro Matos <alejandro.matos@doceleguas.com>
2025-12-04 12:28:26 -0300

    Added a logger service to log messages and errors

--- src/app/api/auth/magic-link/(services)/impl/MagicLinkServiceImpl.ts
+++ src/app/api/auth/magic-link/(services)/impl/MagicLinkServiceImpl.ts
@@ -2,30 +2,36 @@ import { PrismaClient } from "@prisma/client";
 import { RequestStatus } from "@/enums/RequestStatus";
 import crypto from "crypto";
 import { MagicLinkService } from "../MagicLinkService";
+import { LoggerService } from "app/api/logger/LoggerService";
+import { ConsoleLogger } from "app/api/logger/impl/ConsoleLogger";
 
 export class MagicLinkServiceImpl implements MagicLinkService {
-  constructor(private readonly prisma: PrismaClient) {}
+  private readonly logger: LoggerService;
+
+  constructor(private readonly prisma: PrismaClient) {
+    this.logger = new ConsoleLogger(this.constructor.name);
+  }
 
   async generateMagicLink(email: string): Promise<string> {
-    console.log("Generating magic link for", email);
+    this.logger.log("Generating magic link for", email);
     const user = await this.prisma.user.findUnique({ where: { email } });
 
     if (!user) {
       // If no user, just inform the frontend
-      console.log("User not found");
+      this.logger.log("User not found");
       return RequestStatus.USER_NOT_FOUND;
     }
 
     if (!user.approved) {
-      console.log("User not approved");
+      this.logger.log("User not approved");
       return RequestStatus.USER_NOT_APPROVED;
     }
 
-    console.log("User found, generating magic link");
+    this.logger.log("User found, generating magic link");
     const token = crypto.randomBytes(32).toString("hex");
     const expiresAt = new Date(Date.now() + 1000 * 60 * 15); // 15 min
 
-    console.log("Saving magic link");
+    this.logger.log("Saving magic link");
     await this.prisma.magicLink.create({
       data: {
         token,
---
+++ src/app/api/logger/impl/ConsoleLogger.ts
@@ -0,0 +1,16 @@
+import { LoggerService } from "../LoggerService";
+
+export class ConsoleLogger implements LoggerService {
+    loggerClass: string;
+
+    constructor(loggerClass: string) {
+        this.loggerClass = loggerClass;
+    }
+
+    log(message: string, data?: unknown): void {
+        console.log(`INFO ${this.loggerClass}: ${message}`, data || '');
+    }
+    error(message: string, data?: unknown): void {
+        console.error(`ERROR ${this.loggerClass}: ${message}`, data || '');
+    }
+}
