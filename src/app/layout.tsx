8f9f5846920c3a89552bb47cf4c37ea11c3ef9a8
Alejandro Matos <alekosmp86@gmail.com>
2025-12-12 13:53:59 -0300

    Adding Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export default function RootLayout({
@@ -6,8 +7,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className='min-h-screen flex flex-col'>{children}</body>
-    </html>
+    <>
+      <Analytics />
+      <html lang='en'>
+        <body className='min-h-screen flex flex-col'>{children}</body>
+      </html>
+    </>
   );
 }
