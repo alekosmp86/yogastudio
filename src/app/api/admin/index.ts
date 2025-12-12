5a56e5402cca0ea182a1f64d34a59887da0bad6b
Alejandro Matos <alekosmp86@gmail.com>
2025-12-12 15:01:37 -0300

    Fix for production build

--- src/app/api/admin/index.ts
+++ src/app/api/admin/index.ts
@@ -1,6 +1,4 @@
 import { AdminServiceImpl } from "./(services)/impl/AdminServiceImpl";
-import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
 
-export const adminService = new AdminServiceImpl(prisma);
+export const adminService = new AdminServiceImpl();
