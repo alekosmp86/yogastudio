9f30a211d1fd5bfb1310af32e230f737a54c0f0d
Alekos <alekosmp86@gmail.com>
2026-01-02 04:51:25 -0300

    Adding translations to approval page

--- src/components/public/UserApprovalPage.tsx
+++ src/components/public/UserApprovalPage.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import Button from "../shared/Button";
+import { useTranslation } from "react-i18next";
 
 export default function UserApprovalPage() {
+  const { t } = useTranslation();
+
   return (
     <div className='flex flex-col items-center justify-center min-h-screen p-6 bg-theme-headings text-white'>
       <div className='flex flex-col gap-4 w-full max-w-xs bg-theme-cardbg p-6 rounded-lg shadow-xl border border-brand-600'>
         <h1 className='text-xl font-semibold text-center text-primary-800'>
-          Approval
+          {t("approval")}
         </h1>
         <p className='text-center text-primary-800'>
-          Your account is pending approval. Please contact the admin.
+          {t("accountPendingApproval")}
         </p>
 
         <Button
@@ -18,7 +21,7 @@ export default function UserApprovalPage() {
           onClick={() => (window.location.href = "/login")}
           variant='primary'
         >
-          Back to login
+          {t("backToLogin")}
         </Button>
       </div>
     </div>
